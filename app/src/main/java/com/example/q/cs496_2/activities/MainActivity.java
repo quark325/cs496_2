94d29b440e62115c3aba254ebb3e290b850b6a53
euicheon <no1eclee@gmail.com>
2019-01-05 14:52:04 +0900

    FB login on MainActivity

--- app/src/main/java/com/example/q/cs496_2/activities/MainActivity.java
+++ app/src/main/java/com/example/q/cs496_2/activities/MainActivity.java
@@ -1,15 +1,89 @@
 package com.example.q.cs496_2.activities;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.Profile;
+import com.facebook.ProfileTracker;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+import org.json.JSONObject;
 import com.example.q.cs496_2.R;
 
 public class MainActivity extends AppCompatActivity {
+    //FB Login 구현
+    private LoginButton loginButton;
+    private CallbackManager callbackManager;
+    private ProfileTracker  profileTracker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //profileTracker 실행 (무엇을 하는 친구인지 알아봐야함)
+        profileTracker = new ProfileTracker() {
+            @Override
+            protected void onCurrentProfileChanged(Profile oldProfile, Profile currentProfile) {
+                Log.e("profile", String.valueOf(oldProfile));
+            }
+        };
+
         setContentView(R.layout.activity_main);
+
+        callbackManager = CallbackManager.Factory.create();
+        loginButton = (LoginButton)findViewById(R.id.login_button);
+
+        loginButton.setReadPermissions("public_profile","email", "user_birthday", "user_friends");
+
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                //로그인 성공!
+                Log.e("user profile",loginResult.getAccessToken().toString());
+                //얘도 뭐하는 놈인지 모르겠음!!
+                GraphRequest request =GraphRequest.newMeRequest(loginResult.getAccessToken() ,
+                        new GraphRequest.GraphJSONObjectCallback() {
+                            @Override
+                            public void onCompleted(JSONObject object, GraphResponse response) {
+                                try {
+                                    Log.e("user profile",object.toString());
+                                } catch (Exception e) {
+                                    e.printStackTrace();
+                                    //Log.e("now","got it");
+                                }
+                            }
+                        });
+                request.executeAsync();;
+            }
+
+            @Override
+            public void onCancel() {
+                Log.d("TAG","Canceled");
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+                error.printStackTrace();
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
     }
 }
