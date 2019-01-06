889b4ff8e815abae637b073f651f431009f3ddcf
euicheon <no1eclee@gmail.com>
2019-01-06 20:49:03 +0900

    Image Load

--- app/src/main/java/com/example/q/cs496_2/activities/ModifyActivity.java
+++ app/src/main/java/com/example/q/cs496_2/activities/ModifyActivity.java
@@ -1,7 +1,12 @@
 package com.example.q.cs496_2.activities;
 
+import android.content.ActivityNotFoundException;
+import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
@@ -16,6 +21,15 @@ import android.widget.Toast;
 
 import com.example.q.cs496_2.R;
 import com.facebook.Profile;
+import com.koushikdutta.async.future.FutureCallback;
+import com.koushikdutta.ion.Ion;
+import com.koushikdutta.ion.Response;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.File;
+import java.util.concurrent.Future;
 
 import info.hoang8f.android.segmented.SegmentedGroup;
 
@@ -28,12 +42,15 @@ public class ModifyActivity extends AppCompatActivity {
     private String birthday;
     private String residence;
     private RadioButton male, female;
-
+    public File f;
+    ImageView editPhoto;
+    String path;
+    String file_name;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.mypage_modify);
 
-        final ImageView editPhoto = (ImageView) findViewById(R.id.modifyImage);
+        editPhoto = (ImageView) findViewById(R.id.modifyImage);
         final TextView editName = (TextView) findViewById(R.id.modifyName);
         final SegmentedGroup editGender = (SegmentedGroup) findViewById(R.id.segmented);
         final TextView editBirthday = (TextView) findViewById(R.id.modifyBirthDay);
@@ -69,6 +86,13 @@ public class ModifyActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 //TODO PHOTO SELECT 화면으로 넘어가는 기능
+                Intent fintent = new Intent(Intent.ACTION_GET_CONTENT);
+                fintent.setType("image/jpeg");
+                try {
+                    startActivityForResult(fintent, 100);
+                } catch (ActivityNotFoundException e) {
+
+                }
             }
         });
 
@@ -91,6 +115,30 @@ public class ModifyActivity extends AppCompatActivity {
                 }else {
                     gender = "male";
                 }
+
+                //Image Upload
+                //파일이름 : file_name
+                f = new File(path);
+                file_name = f.getName();
+                Future uploading = Ion.with(ModifyActivity.this)
+                        .load("http://143.248.140.106:2980/upload")
+                        .setMultipartFile("image", f)
+                        .asString()
+                        .withResponse()
+                        .setCallback(new FutureCallback<Response<String>>() {
+                            @Override
+                            public void onCompleted(Exception e, Response<String> result) {
+                                try {
+                                    JSONObject jobj = new JSONObject(result.getResult());
+                                    Toast.makeText(getApplicationContext(), jobj.getString("response"), Toast.LENGTH_SHORT).show();
+
+                                } catch (JSONException e1) {
+                                    e1.printStackTrace();
+                                }
+
+                            }
+                        });
+
 
                 /*TODO 여기가 데이터 보내는 부분. 아래있는 형식대로 데이터를 넘기면 된다.
                 ID정보 : id;
@@ -128,7 +176,34 @@ public class ModifyActivity extends AppCompatActivity {
             }
         });
     }
-
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (data == null)
+            return;
+        switch (requestCode) {
+            case 100:
+                if (resultCode == RESULT_OK) {
+                    path = getRealPathFromURI(this,data.getData());
+                    //file_name = f.getName();
+                    Log.d(" Real Path : ", path);
+                    editPhoto.setImageURI(data.getData());
+                    //upload.setVisibility(View.VISIBLE);
+                }
+        }
+    }
+    public String getRealPathFromURI(Context context, Uri contentUri) {
+        Cursor cursor = null;
+        try {
+            String[] proj = { MediaStore.Images.Media.DATA };
+            cursor = context.getContentResolver().query(contentUri,  proj, null, null, null);
+            int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+            cursor.moveToFirst();
+            return cursor.getString(column_index);
+        } finally {
+            if (cursor != null) {
+                cursor.close();
+            }
+        }
+    }
     @NonNull
     private String changeOrder(String birthday) {
         String[] date = birthday.split("/");
